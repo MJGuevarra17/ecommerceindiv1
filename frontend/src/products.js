b25c26ac69544ade39ee8d83fc6fedd308bce1ce
MARTIN <mjguevarra1217@gmail.com>
2023-01-29 14:16:07 +0800

    first commit

---
+++ frontend/src/products.js
@@ -0,0 +1,109 @@
+const products = [
+{
+    '_id': '1',
+    'name': 'Squier Affinity Stratoaster',
+    'image': '/images/1.jpg',
+    'description':
+    'This Strat® features several player-friendly refinements such as a thin and lightweightbody, a slim and comfortable “C”-shaped neck profile, a 2-point tremolo bridge forsuperior tremolo action and sealed die-cast tuning machines with split shafts for smooth,accurate tuning and easy restringing.',
+    'brand': 'Squier by Fender',
+    'category': 'Guitars',
+    'price': 19600,
+    'countInStock': 5,
+    'rating': 4,
+    'numReviews': 12,
+},
+{
+    '_id': '2',
+    'name': 'Audio Technica ATH-M40x Monitor Headphones',
+    'image': '/images/2.jpg',
+    'description':
+    'The M-Series ATH-M40x professional monitor headphones are tuned flat for incredibly accurate audio monitoring across an extended frequency range.',
+    'brand': 'Audio Technica',
+    'category': 'Headphones',
+    'price': 6420,
+    'countInStock': 10,
+    'rating': 5,
+    'numReviews': 8,
+},
+{
+    '_id': '3',
+    'name': 'SHURE SM7B Studio Microphone',
+    'image': '/images/3.jpg',
+    'description':
+    'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    'brand': 'SHURE',
+    'category': 'Microphones',
+    'price': 24355,
+    'countInStock': 3,
+    'rating': 5,
+    'numReviews': 12,
+},
+{
+    '_id': '4',
+    'name': 'Focusrite Scarlett Solo',
+    'image': '/images/4.jpg',
+    'description':
+    'Create studio quality vocals, powerful guitars, luscious keys and huge drums, then mix and master with iconic Focusrite studio hardware - all in the box.',
+    'brand': 'Focusrite',
+    'category': 'Audio Interface',
+    'price': 7410,
+    'countInStock': 11,
+    'rating': 4,
+    'numReviews': 12,
+},
+{
+    '_id': '5',
+    'name': 'Audio-Technica AT2035 Condenser Microphone',
+    'image': '/images/5.jpg',
+    'description':
+    'Designed for home audio recording, music and sound projects, professional studio applications and live performances.',
+    'brand': 'Audio Technica',
+    'category': 'Microphones',
+    'price': 8341,
+    'countInStock': 7,
+    'rating': 4.5,
+    'numReviews': 10,
+},
+{
+    '_id': '6',
+    'name': 'Arturia Minilab MkII',
+    'image': '/images/6.jpg',
+    'description':
+    'MiniLab MkII is the ideal small controller for musicians on the move and studios where workspace is at a premium.',
+    'brand': 'Arturia',
+    'category': 'MIDI Controllers',
+    'price': 5199,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+},
+    {
+    '_id': '7',
+    'name': 'Blue Yeti USB Microphone',
+    'image': '/images/7.jpg',
+    'description':
+    'Four different pickup patterns offer incredible flexibility so you can record vocals for music, podcasts, Twitch streaming, YouTube videos, or even cryptozoology lectures in ways that would normally require multiple microphones.',
+    'brand': 'Blue',
+    'category': 'Microphones',
+    'price': 7695,
+    'countInStock': 0,
+    'rating': 3.5,
+    'numReviews': 12,
+},
+    {
+    '_id': '8',
+    'name': 'Mavies 3-Guitar Stand',
+    'image': '/images/8.jpg',
+    'description':
+    'This can handle a wide range of products such as electric guitars and basses, and acoustic guitars.',
+    'brand': 'Mavies',
+    'category': 'Accessories',
+    'price': 429,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+},
+]
+
+
+export default products
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+function ProductScreen({ match }) {
+  const product = products.find((p) => p._id === match.params.id)
+
+  return (
+    <div>
+      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+      </Row>
+  </div>
+  )
+}
+
+export default ProductScreen
